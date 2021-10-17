e2ba9ffc0c253e6a719461d152d71aaacd890d4f
Arpan Kumar Gorai <82395272+Ethan-Datsun@users.noreply.github.com>
2021-10-17 20:28:56 +0530

    Add files via upload

---
+++ MAXDISTKT.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+bool cmp(pair<ll,ll>&a, pair<ll,ll>&b)
+{
+     return a.second<b.second;
+}
+int main()
+{
+     int testcase;
+     cin>>testcase;
+     while(testcase--)
+     {
+          ll n,x;
+          cin>>n;
+          vector<pair<ll,ll>> arr;
+          
+          for(int i=0;i<n;i++)
+          {
+               cin>>x;
+               arr.push_back(make_pair(x,i));
+          }
+          
+          ll k=0;
+          sort(arr.begin(),arr.end());
+          
+          for(int i=0;i<n;i++)
+          {
+               if(arr[i].first>k)
+               {
+                    arr[i].first=k;
+                    k++;
+               }
+               else if(arr[i].first==k)
+               {
+                    arr[i].first=k;
+               }
+          }
+          sort(arr.begin(),arr.end(),cmp);
+          for(int i=0;i<n;i++)
+          {
+               cout<<arr[i].first<<" ";
+         }
+         
+         cout<<"\n";
+     }
+     return 0;
+}
